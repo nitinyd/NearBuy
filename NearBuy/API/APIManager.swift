fc4ddcb5028dfdb867ddab3dc52b8c6ef4fe2e64
Nitin Yadav <nitin.yadav@saavn.com>
2024-06-01 10:51:23 +0530

    URLSession Code Completed to fetch Data

--- NearBuy/API/APIManager.swift
+++ NearBuy/API/APIManager.swift
@@ -14,6 +14,35 @@ class APIManager {
 
 extension APIManager {
     func getRequest(_ delegate: APIModelProtocol) {
+        let params = delegate.getParams()
+        let endpoint = delegate.getApiEndpoint()
+        let urlRequest = NetworkingUtil.constructAPIRequest(endpoint: endpoint, params: params)
         
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [Any]
+            else {
+                if let error = error {
+                    delegate.fetchFailure(with: error, for: params)
+                    return
+                } else {
+                    let message = ["msg": "Invalid Data"]
+                    let error = NSError(domain: "Error", code: -1, userInfo: message)
+                    delegate.fetchFailure(with: error, for: params)
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                delegate.parse(json: json, params: params)
+                do {
+                    try delegate.parse(data: data, params: params)
+                } catch let parsingError {
+                    delegate.fetchFailure(with: parsingError, for: params)
+                }
+                delegate.fetchSuccess(for: params)
+            }
+        }
+        task.resume()
     }
 }
--- NearBuy/API/NetworkingUtil.swift
+++ NearBuy/API/NetworkingUtil.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 struct NetworkingUtil {
+//https://api.seatgeek.com/2/venues?per_page=10&page=1&client_id=Mzg0OTc0Njl8MTcwMDgxMTg5NC44MDk2NjY5&lat=12.971599&lon=77.594566&range=12mi&q=
     static func constructAPIRequest(endpoint: String, params: [String: Any]) -> URLRequest {
+        var urlString = "https://\(endpoint)?"
         
+        for element in params {
+            urlString.append("&\(element.key)=\(element.value)")
+        }
+        
+        return URLRequest(url: URL(string: urlString)!)
     }
 }
