03db9fe593eb14b877542ec4b459e321eb29e784
Nitin Yadav <nitin.yadav@saavn.com>
2024-06-01 10:43:42 +0530

    Using Custom View Controller instead of Main Storyboard
    1. Removed dependency of Main StoryBoard
    2. Added Custom View Controller to display data

---
+++ NearBuy/ViewControllers/BaseVC.swift
@@ -0,0 +1,15 @@
+//
+//  BaseVC.swift
+//  NearBuy
+//
+//  Created by Nitin Yadav on 1/6/2024.
+//
+
+import Foundation
+import UIKit
+
+class BaseVC: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
--- NearBuy/ViewControllers/RestaurantListVC/RestaurantListVC.swift
+++ NearBuy/ViewControllers/RestaurantListVC/RestaurantListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RestaurantListVC.swift
 //  NearBuy
 //
 //  Created by Nitin Yadav on 1/6/2024.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantListVC: BaseVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
